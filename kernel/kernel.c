0fa52636865e10155668eb26929de65219a59b74
rilmazafone <107496452+rilmazafone@users.noreply.github.com>
2026-03-15 20:29:21 +0100

    Add files via upload

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,11 @@
-#include "../drivers/screen.h"
-#include "util.h"
 #include "../cpu/isr.h"
-#include "../cpu/idt.h"
+#include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main() {
     isr_install();
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
+
+    asm volatile("sti");
+    init_timer(50);
+    init_keyboard();
 }
--- kernel/util.c
+++ kernel/util.c
@@ -1,21 +1,43 @@
-void memory_copy(char *source, char *dest, int nbytes){
-    for(int i=0;i<nbytes;i++){
-        *(dest+i) = *(source + i);
+#include "util.h"
+
+void memory_copy(char *source, char *dest, int nbytes) {
+    int i;
+    for (i = 0; i < nbytes; i++) {
+        *(dest + i) = *(source + i);
     }
 }
 
-void int_to_ascii(int n, char str[]){
-    int sign = n;
-    int i = 0;
+void memory_set(u8 *dest, u8 val, u32 len) {
+    u8 *temp = (u8 *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
 
-    if (sign < 0){
-        n = -n;
-    }
 
+void int_to_ascii(int n, char str[]) {
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
     do {
         str[i++] = n % 10 + '0';
     } while ((n /= 10) > 0);
 
     if (sign < 0) str[i++] = '-';
     str[i] = '\0';
+
+    reverse(str);
+}
+
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
 }
